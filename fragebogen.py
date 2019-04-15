b82c73085e958a1cd817aaa51578a17316b1ad3f
Thorsten Kamp <20558077+Thokam@users.noreply.github.com>
2019-04-15 12:27:28 +0200

    added hard questions and corresponding parameters

--- fragebogen.py
+++ fragebogen.py
@@ -4,7 +4,9 @@ import sys
 from docx import Document
 import argparse
 
-#special rules 
+#special rules
+ 
+ 
 def anzahl(thema):
     anzahl = 0
 
@@ -57,10 +59,33 @@ def get_questions(filename):
         temp_fragen.append(thema_fragen)
     return temp_fragen
 
+def get_hard_questions(filename_hard):
+    temp_fragen = get_questions(filename_hard)
+    result=[]
+    used = 0
+    used_qpt = []
+
+    for x in range(0, len(temp_fragen)):
+        used_qpt.append(0)
+    while used <args.number_qh:
+        t = random.randint(0, len(temp_fragen)-1)
+        if used_qpt[t] < anzahl(t):
+            output = random.sample(temp_fragen[t], 1)
+            if result.count(output[0])==0:
+                result.append(output[0])
+                used +=1
+                used_qpt[t] +=1  
+
+    return result, used_qpt
+
 def create_fragebogen():
 
     #All questions. One sublist per topic. Topics are seperated by '###'
     all_questions = []
+
+    #used questions per topic 
+    used_qpt = {}
+    used_q=0
 
     #random id. Unique for questionnaire (as text and docx) and answerfile  
     id = random.randint(1,100000000)
@@ -70,11 +95,15 @@ def create_fragebogen():
     #subset of all_questions
     random_questions = []
 
+    
+    if args.filename_hard != "0":
+        random_questions,used_qpt = get_hard_questions(args.filename_hard)
+
     for topic in range(0, len(all_questions)):
         if args.number_q != -1:
             number_questions=args.number_q
         else: 
-            number_questions = anzahl(topic)
+            number_questions = anzahl(topic)-used_qpt[topic]
 
         if len(all_questions[topic]) > 0:
             output = random.sample(all_questions[topic], number_questions)
@@ -82,7 +111,7 @@ def create_fragebogen():
                 random_questions.append(item)       
 
     #debug-output
-    pprint.pprint(random_questions)
+    #pprint.pprint(random_questions)
 
 
     #create questionnaire and a file named 'check' with the correct answers in the correct order 
@@ -99,6 +128,7 @@ def create_fragebogen():
                 out.write("\n")
                 control.write(f+": "+(str)(item.split("?")[1].split("#")[1]))
 
+    #TODO Switch to docxtpl and use a template?
     #copy questionnaire to a docx-file
     with open(args.output_file+"_"+str(id)+".txt", "r") as input:
         content =input.read()
@@ -118,7 +148,9 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--filename', type=str, default="fragen.txt", help='file with the questions. Default=fragen.txt')
     parser.add_argument('-n', '--number_q', type=int, default=1, help='number of questions per topic. Default=1. Use -1 for the special rules')
     parser.add_argument('-o', '--output_file', type=str, default="antwort", help='filename-prefix for the questionnaire. Default=antwort')
-
+    parser.add_argument('-qh', '--number_qh', type=int, default=0, help='Number of hard questions. Default = 0')
+    parser.add_argument('-fh', '--filename_hard', type=str, default="0", help='file with the hard questions.')
+    
 
     args=parser.parse_args()
 
